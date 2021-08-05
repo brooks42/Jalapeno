1b3ce45e4aeaea53e2af10d9eafbdfbf3c7f24b1
brooks42 <britainsux@gmail.com>
2021-08-04 20:22:48 -0700

    remove build directory

--- jalapeno-back/build/routes/sensors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SensorRouter = void 0;
-const express_1 = require("express");
-exports.SensorRouter = express_1.Router();
-exports.SensorRouter.get('/', function (req, res) {
-    res.send('Express + TypeScript Server from a route :)');
-});
